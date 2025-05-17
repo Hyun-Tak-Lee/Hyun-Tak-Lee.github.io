f8842bf488946231b4541c5a79eb1be553d4a7a4
Lee-HT <12gusxkr@naver.com>
2025-05-17 13:50:42 +0900

    astro 컨텐츠 컬렉션으로 post 를 markdown 으로 작성 가능하도록 구현현

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,5 @@ import react from "@astrojs/react";
 export default defineConfig({
   site: "https://hyun-tak-lee.github.io",
   base: "/",
-  integrations: [react()]
+  integrations: [react()],
 });
---
+++ src/content/config.ts
@@ -0,0 +1,20 @@
+import { defineCollection, z } from 'astro:content';
+
+const blogCollection = defineCollection({
+  type: 'content',
+  schema: z.object({
+    category: z.string(),
+    title: z.string(),
+    pubDate: z.date(),
+    description: z.string(),
+    author: z.string(),
+    image: z.object({
+      url: z.string(),
+      alt: z.string()
+    }).optional(),
+  })
+});
+
+export const collections = {
+  'blog': blogCollection
+}; 
