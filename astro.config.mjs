698433e1202d703f1205c7a2844f0d2b2b2c976f
Hyun-Tak-Lee <12gusxkr@naver.com>
2026-06-24 21:12:11 +0900

    feat: 문서 시스템 기본 구조 구축 및 사이트맵 연동, 기술/프로젝트 문서용 베이스 레이아웃 및 공통 컴포넌트 구현, 카테고리 및 슬러그 기반의 동적 문서 페이지 구성, SEO 최적화를 위한 동적 사이트맵 연동

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,13 @@
 import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
 import tailwindcss from "@tailwindcss/vite";
+import sitemap from "@astrojs/sitemap";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://hyun-tak-lee.github.io",
   base: "/",
-  integrations: [react()],
+  integrations: [react(), sitemap()],
 
   vite: {
     plugins: [tailwindcss()],
